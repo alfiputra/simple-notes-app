0e293ea8d77fc89a8c437aac4ed04d29d6674116
alfiputra <alfiputra.id@gmail.com>
2021-12-26 17:37:42 +0700

    Finished

--- app/src/main/java/id/alfiputra/notesapp/NoteRepository.kt
+++ app/src/main/java/id/alfiputra/notesapp/NoteRepository.kt
@@ -5,15 +5,15 @@ import androidx.lifecycle.LiveData
 class NoteRepository(private val notesDao: NotesDao) {
     val allNotes: LiveData<List<Note>> = notesDao.getAllNotes()
 
-    suspend fun insert(note: Note){
+    fun insert(note: Note){
         notesDao.insert(note)
     }
 
-    suspend fun update(note: Note){
+    fun update(note: Note){
         notesDao.update(note)
     }
 
-    suspend fun delete(note: Note){
+    fun delete(note: Note){
         notesDao.delete(note)
     }
 
